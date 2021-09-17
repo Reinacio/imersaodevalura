68844a0ccc31b457616217a07717f13b23108543
Regiane Inacio <regianehinacio@gmai.com>
2021-09-17 17:09:44 -0300

    subindo o código

---
+++ script.js
@@ -0,0 +1,18 @@
+var listaFilmes = [
+    "https://br.web.img3.acsta.net/pictures/21/07/02/11/47/1576324.jpg",
+    "https://media.fstatic.com/_GgIiYdEj0yvVFqjUDPje9G4UcM=/290x478/smart/media/movies/covers/2020/04/91dvJVKJsOL._SL1500_.jpg",
+    "https://br.web.img2.acsta.net/c_310_420/pictures/21/07/09/21/18/3544259.jpg",
+    "https://lh3.googleusercontent.com/proxy/WpQVfeY4ct7RBgIhsDeXrcen1c3vbfM1HmJZ_3fne8gVsAEqfrV9H64A7xOv4bnPEGIRF-78AnX0MaLrf4pk8uuahVmf5A0Q22WQ117Lh3E01JkssWS469Sf2SLvsOGY9peU9S3r64J9p7fb",
+    "https://br.web.img2.acsta.net/pictures/21/05/11/09/03/5865537.jpg",
+    "https://br.web.img3.acsta.net/pictures/16/10/13/21/27/038061.jpg",
+    "https://upload.wikimedia.org/wikipedia/pt/thumb/6/63/Joker_%282019%29.jpg/250px-Joker_%282019%29.jpg",
+    "https://br.web.img3.acsta.net/pictures/16/05/18/23/10/260583.jpg",
+    "https://leiturafilmica.com.br/wp-content/uploads/2021/04/radioactive-poster.jpg",
+    "https://1.bp.blogspot.com/-ERGIM0M8Gbo/YQ06YXuDGAI/AAAAAAAAsmY/AneqCxfxXnsVkCFBO4F3Xbo5r1XSnyEhACLcBGAsYHQ/s900/A%2BJornada%2Bde%2BVivo%2Bnetflix%2Bposter.jpg",
+    "https://br.web.img3.acsta.net/pictures/19/04/05/20/54/1435524.jpg"
+  ];
+
+  // pesquisar fo it ; while
+  for (var indice = 0; indice < listaFilmes.length; indice++) {
+    document.write("<img src=" + listaFilmes[indice] + ">");
+  }
